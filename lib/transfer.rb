caea26c3e74b94f654588eebe524f11844b4befd
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-08 01:38:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :status
+  attr_reader :sender, :receiver
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  # def execute_transaction
+  #
+  # end
+  #
+  # def reverse_transfer
+  #
+  # end
+
 end
